73b260dd92fcd5034e33121570de982070228899
camille-eloah <camelogithub@gmail.com>
2025-05-25 00:53:37 -0300

    feat - add animação de correr, refatorei o código do player

--- Player.cs
+++ Player.cs
@@ -1,17 +1,20 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Mono.Helper;
+using SharpDX.Win32;
 
 namespace GameDuMouse
 {
     public class Player
     {
-        private Animation idleAnime;
+        private Animation idleAnime, runAnime,player;
         private Game game;
         private Vector2 wallRight, wallLeft;
         private float personY;
+        private bool positionInit = false;
         public Player(Game game)
         {
             this.game = game;
@@ -21,7 +24,7 @@ namespace GameDuMouse
         {
             personY = 270;
             wallRight = new Vector2(200, personY);
-            wallLeft = new Vector2(-130, personY);
+            wallLeft = new Vector2(0, personY);
         }
 
         public void LoadContent(ContentManager content)
@@ -30,42 +33,75 @@ namespace GameDuMouse
             idleAnime.AddSprite("Idle/Idle01", "Idle/Idle02", "Idle/Idle03",
                                 "Idle/Idle04", "Idle/Idle05", "Idle/Idle06");
             idleAnime.Position = new Vector2(100, personY);
+
+            runAnime = new Animation(game, 50f);
+            runAnime.AddSprite("run/run01", "run/run02", "run/run03",
+                                "run/run04", "run/run05", "run/run06", "run/run07");
+            runAnime.Position = new Vector2(100, personY);
+
+            player = idleAnime;
         }
 
         public void Move(GameTime gameTime, Background background)
         {
+
             var KeyboardState = Keyboard.GetState();
+            idleAnime.Position = player.Position;
             if (KeyboardState.IsKeyDown(Keys.D))
             {
-                idleAnime.Position += new Vector2(10, 0);
-                idleAnime.Effects = SpriteEffects.None;
-                if (idleAnime.Position.X >= wallRight.X)
-                {
-                    idleAnime.Position = wallRight;
-                    background.ScrollLeft();
-                }
-
+                MoveRight(background);
+                positionInit = false;
             }
             else if (KeyboardState.IsKeyDown(Keys.A))
             {
-                idleAnime.Position += new Vector2(-10, 0);
-                idleAnime.Effects = SpriteEffects.FlipHorizontally;
-                if (idleAnime.Position.X <= wallLeft.X)
-                {
-                    idleAnime.Position = wallLeft;
-                    background.ScrollRight();
-                }
-
+                MoveLeft(background);
+                positionInit = true;
             }
+            else if (positionInit == false)
+            {
+                player = idleAnime;
+                player.Effects = SpriteEffects.None;
+            }
+            else if (positionInit == true)
+            {
+                player = idleAnime;
+                player.Effects = SpriteEffects.FlipHorizontally;
+            }
+            
+            
+        }
+        private void MoveRight(Background background)
+        {
+            player = runAnime;
+            player.Position += new Vector2(10, 0);
+            player.Effects = SpriteEffects.None;
+            if (player.Position.X >= wallRight.X)
+            {
+                player.Position = wallRight;
+                background.ScrollLeft();
+            }
+            Console.WriteLine($"Posição do player: {player.Position}");
+        }
+        private void MoveLeft(Background background)
+        {
+            player = runAnime;
+            player.Position += new Vector2(-10, 0);
+            player.Effects = SpriteEffects.FlipHorizontally;
+            if (player.Position.X <= wallLeft.X)
+            {
+                player.Position = wallLeft;
+                background.ScrollRight();
+            }
+            Console.WriteLine($"Posição do player: {player.Position}");
         }
 
         public void Update(GameTime gameTime)
         {
-            idleAnime.Update(gameTime);
+            player.Update(gameTime);
         }
         public void Draw(GameTime gameTime)
         {
-            idleAnime.Draw(gameTime);
+            player.Draw(gameTime);
         }
 
     }

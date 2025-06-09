f23d98f6e2997f2f1dbd2c5403d7edbfe774a99f
Ermesondesu <ermesondesu@gmail.com>
2025-06-09 17:30:20 -0300

    add set{} no Collider do player

--- Player.cs
+++ Player.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -12,18 +13,29 @@ namespace GameDuMouse
 
         private Animation idleAnime, runAnime, trans_run, trans_idle;
         private AnimationController animationController;
-        private KeyboardState previousKeyboardState,keyboardState ;
+        private KeyboardState previousKeyboardState, keyboardState;
         private Game game;
         private Vector2 velocity;
         private float groundY, gravity, jumpStrength;
         private bool isGrounded;
-        private Rectangle groundCollider,rightBarrerCollider, leftBarrerCollider;
+        private int widthColliderPlayer,heightColliderPlayer;
+        private Rectangle groundCollider, rightBarrerCollider, leftBarrerCollider;
+        private Rectangle? manualCollider;
         private Rectangle Collider
         {
             get
             {
+                widthColliderPlayer = 100;
+                heightColliderPlayer = 110;
                 var pos = animationController.Position;
-                return new Rectangle((int)pos.X, (int)pos.Y, 100, 110);
+                if (manualCollider.HasValue)
+                    return manualCollider.Value;
+
+                return new Rectangle((int)pos.X, (int)pos.Y, widthColliderPlayer, heightColliderPlayer);
+            }
+            set
+            {
+                manualCollider = value;
             }
         }
 
@@ -34,7 +46,7 @@ namespace GameDuMouse
         }
         public Vector2 GetPosition()
         {
-            Vector2 positionPlayer = new Vector2(Collider.X,300);
+            Vector2 positionPlayer = new Vector2(Collider.X, 300);
             return positionPlayer;
         }
         public void Initialize()
@@ -42,7 +54,7 @@ namespace GameDuMouse
             groundY = 400;
             animationController = new AnimationController();
             gravity = 1f;
-            jumpStrength = -15f;
+            jumpStrength = -18f;
             groundCollider = new Rectangle(0, (int)groundY, 6650, 50);
             rightBarrerCollider = new Rectangle(6650, 1, 10, 500);
             leftBarrerCollider = new Rectangle(10, 1, 10, 500);
@@ -51,8 +63,8 @@ namespace GameDuMouse
 
         public void LoadContent(ContentManager content)
         {
-            debugTexture = new Texture2D(game.GraphicsDevice,1,1);
-            debugTexture.SetData(new[]{ Color.White});
+            debugTexture = new Texture2D(game.GraphicsDevice, 1, 1);
+            debugTexture.SetData(new[] { Color.White });
 
             idleAnime = new Animation(game, 170f);
             idleAnime.AddSprite("Idle/Idle01", "Idle/Idle02", "Idle/Idle03",
@@ -82,58 +94,57 @@ namespace GameDuMouse
 
         public void Move()
         {
-            bool SpacePressed = keyboardState.IsKeyDown(Keys.Space) &&
-                                previousKeyboardState.IsKeyUp(Keys.Space);
-            
+            bool SpacePressed = keyboardState.IsKeyDown(Keys.Space);
+
             if (SpacePressed && isGrounded)
             {
                 velocity.Y = jumpStrength;
                 isGrounded = false;
             }
-            
+
             if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
-                {
-                    animationController.Effects = SpriteEffects.None;
+            {
+                animationController.Effects = SpriteEffects.None;
 
-                    if (animationController.CurrentState != PlayerState.Running &&
-                       animationController.CurrentState != PlayerState.TransitionToRun)
-                    {
-                        animationController.StartTransition(PlayerState.TransitionToRun, 80);
-                    }
-                    if (animationController.CurrentState == PlayerState.Running)
-                    {
-                        MoveRight();
-                    }
-                }
-                else if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
+                if (animationController.CurrentState != PlayerState.Running &&
+                   animationController.CurrentState != PlayerState.TransitionToRun)
                 {
-                    animationController.Effects = SpriteEffects.FlipHorizontally;
-
-                    if (animationController.CurrentState != PlayerState.Running &&
-                        animationController.CurrentState != PlayerState.TransitionToRun)
-                    {
-                        animationController.StartTransition(PlayerState.TransitionToRun, 80);
-                    }
-
-                    if (animationController.CurrentState == PlayerState.Running)
-                    {
-                        MoveLeft();
-                    }
+                    animationController.StartTransition(PlayerState.TransitionToRun, 80);
                 }
-                else
+                if (animationController.CurrentState == PlayerState.Running)
                 {
-                    if (animationController.CurrentState != PlayerState.Idle &&
-                       animationController.CurrentState != PlayerState.TransitionToIdle)
-                    {
-                        animationController.StartTransition(PlayerState.TransitionToIdle, 80);
-                    }
+                    MoveRight();
                 }
+            }
+            else if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
+            {
+                animationController.Effects = SpriteEffects.FlipHorizontally;
+
+                if (animationController.CurrentState != PlayerState.Running &&
+                    animationController.CurrentState != PlayerState.TransitionToRun)
+                {
+                    animationController.StartTransition(PlayerState.TransitionToRun, 80);
+                }
+
+                if (animationController.CurrentState == PlayerState.Running)
+                {
+                    MoveLeft();
+                }
+            }
+            else
+            {
+                if (animationController.CurrentState != PlayerState.Idle &&
+                   animationController.CurrentState != PlayerState.TransitionToIdle)
+                {
+                    animationController.StartTransition(PlayerState.TransitionToIdle, 80);
+                }
+            }
         }
         private void MoveRight()
         {
             var position = animationController.Position;
             var nextPosition = position + new Vector2(10, 0);
-            var futureCollider = new Rectangle((int)nextPosition.X,(int)nextPosition.Y,100,110);
+            var futureCollider = new Rectangle((int)nextPosition.X, (int)nextPosition.Y, 100, 110);
 
             if (futureCollider.Intersects(rightBarrerCollider))
             {
@@ -146,7 +157,7 @@ namespace GameDuMouse
         {
             var position = animationController.Position;
             var nextPosition = position + new Vector2(-10, 0);
-            var futureCollider = new Rectangle((int)nextPosition.X,(int)nextPosition.Y,100,110);
+            var futureCollider = new Rectangle((int)nextPosition.X , (int)nextPosition.Y, 100, 110);
 
             if (futureCollider.Intersects(leftBarrerCollider))
             {

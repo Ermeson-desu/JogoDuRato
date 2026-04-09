1d06b0c90bc2aace952f37d6d2e849606ee969a7
Ermesondesu <ermesondesu@gmail.com>
2026-04-09 17:56:13 -0300

    refactor: Simplificar CreateObstacleScreen para tela genérica minimalista
    - Remover toda lógica complexa de Texture, states e rendering customizado
    - Manter apenas título e botão de voltar (BackButton)
    - Tela limpa e pronta para extensão futura
    - Seguir padrão simples como Settings

--- GameMain/UI/CreateObstacleScreen.cs
+++ GameMain/UI/CreateObstacleScreen.cs
@@ -1,99 +1,47 @@
-using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using GameDuMouse.GameMain.Core;
 using GameDuMouse.GameMain.Input;
-using GameDuMouse.GameMain.Rendering;
 using GameDuMouse.GameMain.UI.Components;
 
 namespace GameDuMouse.GameMain.UI
 {
     public class CreateObstacleScreen
     {
-        private const int Margin = 10;
-
         private Game game;
         private SpriteFont font;
         private BackButton backButton;
-        private Texture2D pixel;
         private InputManager inputManager;
-
-        private MouseState previousMouse;
-        private KeyboardState previousKeyboard;
-
-        private int screenWidth;
-        private int screenHeight;
 
         public CreateObstacleScreen(Game game)
         {
             this.game = game;
             inputManager = game.Services.GetService(typeof(InputManager)) as InputManager;
-            previousMouse = inputManager != null ? inputManager.Mouse : Mouse.GetState();
-            previousKeyboard = inputManager != null ? inputManager.Keyboard : Keyboard.GetState();
         }
 
         public void ResetInput()
         {
-            previousMouse = inputManager != null ? inputManager.Mouse : Mouse.GetState();
-            previousKeyboard = inputManager != null ? inputManager.Keyboard : Keyboard.GetState();
+            backButton?.ResetInput();
         }
 
         public void LoadContent(Microsoft.Xna.Framework.Content.ContentManager content)
         {
             font = content.Load<SpriteFont>("Font/Arial");
             backButton = new BackButton(font, inputManager);
-            
-            screenWidth = game.GraphicsDevice.Viewport.Width;
-            screenHeight = game.GraphicsDevice.Viewport.Height;
-
-            var textureCache = game.Services.GetService(typeof(TextureCache)) as TextureCache;
-            if (textureCache != null)
-            {
-                pixel = textureCache.Pixel;
-            }
-            else
-            {
-                // Create a fallback 1x1 white pixel if TextureCache is not available
-                pixel = new Texture2D(game.GraphicsDevice, 1, 1);
-                pixel.SetData(new[] { Color.White });
-            }
         }
 
         public void Update(StateManager stateManager)
         {
-            // Back button: if it changes the state we should bail out
             backButton?.Update(stateManager);
-            if (stateManager.CurrentState != GameState.CreatingObstacle)
-                return;
-
-            var mouse = inputManager != null ? inputManager.Mouse : Mouse.GetState();
-            var keyboard = inputManager != null ? inputManager.Keyboard : Keyboard.GetState();
-
-            // TODO: Implementar lógica de criação de obstáculo
-
-            previousMouse = mouse;
-            previousKeyboard = keyboard;
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            // Background
-            spriteBatch.Draw(pixel, new Rectangle(0, 0, screenWidth, screenHeight), Color.DarkSlateGray);
-
-            // Title
             string title = "Criar Novo Obstáculo";
             var titleSize = font.MeasureString(title);
-            var titlePos = new Vector2((screenWidth - titleSize.X) / 2f, Margin + 20);
+            var titlePos = new Vector2((game.GraphicsDevice.Viewport.Width - titleSize.X) / 2f, 50);
             spriteBatch.DrawString(font, title, titlePos, Color.White);
 
-            // Placeholder message
-            string placeholder = "Tela de criação de obstáculos (Em desenvolvimento)";
-            var placeholderSize = font.MeasureString(placeholder);
-            var placeholderPos = new Vector2((screenWidth - placeholderSize.X) / 2f, screenHeight / 2 - placeholderSize.Y / 2);
-            spriteBatch.DrawString(font, placeholder, placeholderPos, Color.Yellow);
-
-            // Draw back button
             backButton?.Draw(spriteBatch);
         }
     }
